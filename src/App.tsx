22906eafd3fa323ba40a189620165e411753f2e6
Vitaliy Obyedov <obyedov@gmail.com>
2023-02-04 11:51:41 +0100

    add state/data and inserted the Todolist component

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,27 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {TaskType, Todolist} from "./Todolist";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    let tasks1: Array<TaskType> = [
+        { id: 1, title: 'CSS', isDone : true },
+        { id: 2, title: 'JS', isDone : true },
+        { id: 3, title: 'React', isDone : false }
+    ]
+
+    let tasks2: Array<TaskType> = [
+        { id: 1, title: 'Interstellar', isDone : true },
+        { id: 2, title: 'XXX', isDone : false },
+        { id: 3, title: 'Tenet', isDone : true }
+    ]
+
+    return (
+        <div className="App">
+            <Todolist title='what to learn' tasks={tasks1}/>
+            <Todolist title='movies' tasks={tasks2}/>
+        </div>
+    );
 }
 
 export default App;
